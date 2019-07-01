d5bc62baf9b53d674505f188be7cc6e60f99949d
aniketdvd <dwi.aniket@gmail.com>
2019-07-02 00:24:08 +0530

    Updates

---
+++ precache-manifest.bb213d4ce936b6645eba4704ea573156.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8857e332d363adb3f07bddf2ceb206",
+    "url": "/reactodo/index.html"
+  },
+  {
+    "revision": "f10b24300f9774a5ceb9",
+    "url": "/reactodo/static/css/main.7fba6859.chunk.css"
+  },
+  {
+    "revision": "e7d63dd52f49bc41ab85",
+    "url": "/reactodo/static/js/2.3c33110d.chunk.js"
+  },
+  {
+    "revision": "f10b24300f9774a5ceb9",
+    "url": "/reactodo/static/js/main.450068b0.chunk.js"
+  },
+  {
+    "revision": "05b3bcf9592b26d303bb",
+    "url": "/reactodo/static/js/runtime~main.fdba5a20.js"
+  }
+]);
